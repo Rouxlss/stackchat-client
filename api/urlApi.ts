3fe142d4c4ed34358154d4736130dd2037ef075b
Rouxls <adolfo.flores@focusservices.com>
2022-05-22 06:51:09 -0600

    messages and auth

--- api/urlApi.ts
+++ api/urlApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const stackChatApi = axios.create({
-    // baseURL: 'https://stackchat-server.herokuapp.com/api'
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'https://stackchat-server.herokuapp.com/api'
+    // baseURL: 'http://localhost:4000/api'
 })
 
 export default stackChatApi;
